8a597cddb94d067c2f4ea212cb347c1c7f9e51b5
Guillaume Lambert <guillaume.p.lambert@gmail.com>
2020-03-01 20:50:28 +0100

    feat(App) working mvp

---
+++ colors.js
@@ -0,0 +1,20 @@
+// from https://clrs.cc/
+export const colors = {
+    navy: "#001f3f",
+    blue: "#0074D9",
+    aqua: "#7FDBFF",
+    teal: "#39CCCC",
+    olive: "#3D9970",
+    green: "#2ECC40",
+    lime: "#01FF70",
+    yellow: "#FFDC00",
+    orange: "#FF851B",
+    red: "#FF4136",
+    maroon: "#85144b",
+    fuschia: "#F012BE",
+    purple: "#B10DC9",
+    black: "#111111",
+    gray: "#AAAAAA",
+    silver: "#DDDDDD",
+    white: "#FFFFFF"
+}
---
+++ pages/index.js
@@ -0,0 +1,108 @@
+import { useRouter } from 'next/router';
+import useSWR, { mutate } from 'swr';
+import { colors } from '../colors.js';
+import { useState } from "react";
+import Header from '../components/Header'
+
+function fetcher(url) {
+  return fetch(url).then(r => r.json());
+}
+
+export default function Index() {
+  const [ reveal, setReveal ] = useState(false);
+  const { query } = useRouter();
+  const { data, error } = useSWR(
+    `/api/randomQuote${query.movie ? '?movie=' + query.movie : ''}`,
+    fetcher
+  );
+  // The following line has optional chaining, added in Next.js v9.1.5,
+  // is the same as `data && data.movie`
+  const movie = data?.movie;
+  const year = data?.year;
+  let quote = data?.quote;
+
+  const revealAnswer = () => {
+    setReveal(true)
+  }
+
+  const fetchNewQuote = () => {
+    setReveal(false)
+    mutate(`/api/randomQuote`)
+  }
+
+  if (!data) quote = 'Loading...';
+  if (error) quote = 'Failed to fetch the quote.';
+
+
+  return (
+    <main className="center">
+      <Header />
+      <div className="quoteContainer">
+        <div className="quote">{quote}</div>
+        {reveal && movie ? <span className="movie">{`${movie} (${year})`}</span> : '...'}
+      </div>
+      <div className="footer">
+        <div className={reveal ? "textButton disbled" : "textButton" } onClick={revealAnswer}>Which movie is this taken from?</div>
+        <div className="textButton" onClick={fetchNewQuote}>New random quote</div>
+      </div>
+      
+
+      <style jsx global>{`
+        html, body {
+          height: 100vh; 
+          margin: 0;
+          font-family: Poppins;
+        }
+        #__next { 
+          height: 100vh; 
+          background-color: ${colors.navy};
+          display: flex;
+          align-items: center;
+          justify-content: center;
+        }
+      `}</style>
+
+      <style jsx>{`
+        main {
+          display: flex;
+          justify-content: space-between;
+          flex-direction: column;
+          height: 100vh;
+          text-align: center;
+          background-color: ${colors.navy};
+          color: ${colors.white}
+        }
+        .quote {
+          font-size: 32px;
+          padding-bottom: 10px;
+          margin: 0px 20px;
+        }
+        .movie {
+          font-size: 18px;
+        }
+        .textButton {
+          background-color: ${colors.white};
+          color: ${colors.navy};
+          border-radius: 8px;
+          padding: 8px;
+          margin: 12px;
+          font-size: 18px;
+          cursor: pointer;
+          text-decoration: none;
+          width: -moz-fit-content;
+        }
+        .disbled {
+          cursor: auto;
+          background-color: transparent;
+          color: ${colors.gray};
+        }
+        .footer {
+          display: flex;
+          flex-direction: row;
+          padding-bottom: 32px;
+          justify-content: center;
+        }
+      `}</style>
+    </main>
+  );
+}
